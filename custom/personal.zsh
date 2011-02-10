405ba29df7cd46ac4116f4824e2fb23d537c3d89
JonathonW <gandalf72487@yahoo.com>
2011-02-09 22:04:33 -0600

    Add personal zsh settings file (shouldn't be pulled into other peoples' repositories)

---
+++ custom/personal.zsh
@@ -0,0 +1,16 @@
+# Some personal preferences I want to share across all my machines:
+
+# Default editor (for git commit messages, visudo, etc.) should be nano
+export EDITOR=nano
+
+# Make default ls colors sane (similar to Linux)
+export LSCOLORS=ExGxcxdxCxegedabagacad
+
+# Aliases to lots of pretty multicolored xterms
+alias xterm1='xterm -bg black -fg orange -sb'
+alias xterm2='xterm -bg black -fg yellow -sb'
+alias xterm3='xterm -bg black -fg green -sb'
+alias xterm4='xterm -bg black -fg cyan -sb'
+alias xterm5='xterm -bg black -fg white -sb'
+alias xterm6='xterm -bg black -fg DodgerBlue -sb'
+alias xterm7='xterm -bg black -fg red -sb'
