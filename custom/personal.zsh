1f011588b14bb2a702a1544adaca320b8ffbbf73
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2015-05-20 14:48:55 -0500

    Add numpad keys to personal.zsh

--- custom/personal.zsh
+++ custom/personal.zsh
@@ -14,3 +14,26 @@ alias xterm4='xterm -bg black -fg cyan -sb'
 alias xterm5='xterm -bg black -fg white -sb'
 alias xterm6='xterm -bg black -fg DodgerBlue -sb'
 alias xterm7='xterm -bg black -fg red -sb'
+
+# Keypad
+# 0 . Enter
+bindkey -s "^[Op" "0"
+bindkey -s "^[On" "."
+bindkey -s "^[OM" "^M"
+# 1 2 3
+bindkey -s "^[Oq" "1"
+bindkey -s "^[Or" "2"
+bindkey -s "^[Os" "3"
+# 4 5 6
+bindkey -s "^[Ot" "4"
+bindkey -s "^[Ou" "5"
+bindkey -s "^[Ov" "6"
+# 7 8 9
+bindkey -s "^[Ow" "7"
+bindkey -s "^[Ox" "8"
+bindkey -s "^[Oy" "9"
+# + -  * /
+bindkey -s "^[Ol" "+"
+bindkey -s "^[Om" "-"
+bindkey -s "^[Oj" "*"
+bindkey -s "^[Oo" "/"
